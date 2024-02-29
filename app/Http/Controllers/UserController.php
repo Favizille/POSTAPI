f33e18cf03577557d412ebd0b9ed1eb5a1a798a8
unknown <favourgabriel50@gmail.com>
2024-02-29 15:44:17 +0100

    completed profile CRUD functionalities

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     protected $profile;
+    protected $user;
 
-    public function __construct(Profile $profile){
+    public function __construct(Profile $profile, User $user){
         $this->profile = $profile;
+        $this->user = $user;
     }
 
     public function createProfile(Request $request){
 
-        $profileDetails = $request->validate([
+        $request->validate([
             'sex' => 'required',
             'dob' => 'required',
             'hobby' => 'required',
             'status'  => 'required',
-            'occupation'  => 'required'
+            'occupation'  => 'required',
+            'user_id'  => 'required'
         ]);
+
+        $profileDetails = [
+            'sex' => $request->input('sex'),
+            'dob' => $request->input('dob'),
+            'hobby' => $request->input('hobby'),
+            'status' => $request->input('status'),
+            'occupation' => $request->input('occupation'),
+            'user_id' => $request->input('user_id')
+        ];
 
         if(!$this->profile->create($profileDetails)){
             return [
@@ -60,4 +73,68 @@ class UserController extends Controller
             'message' => 'Profile created successfully'
         ];
     }
+
+
+    public function getProfile($profileId){
+
+        if(!$profile = $this->profile->find($profileId)){
+            return [
+                'status' => 'failed',
+                'status_code' => 400,
+                'message' => 'Could not get User Profile'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => $profile
+        ];
+    }
+
+    public function updateProfile($profileId, Request $request){
+        if(!$profile = $this->profile->find($profileId)){
+            return [
+                'status' =>'failed',
+                'status_code' => 400,
+                'message' => 'Failed to Update',
+            ];
+        }
+
+        $profileDetails = [
+            'sex'=> $request->sex,
+            'dob'=> $request->dob,
+            'hobby'=> $request->hobby,
+            'status'=> $request->status,
+            'occupation'=> $request->occupation,
+        ];
+
+
+        $profile->update($profileDetails);
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'data' => $profile,
+        ];
+    }
+
+    public function deleteProfile($profileId){
+        $profile = $this->profile->find($profileId);
+
+        if(!$profile){
+            return [
+                'status' =>'failed',
+                'message' => 'Could not find Profile',
+            ];
+        }
+
+        $profile->delete();
+
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'message' => 'Profile Deleted',
+        ];
+    }
+
 }
