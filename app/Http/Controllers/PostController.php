7b51f052d8bc82f6637c05ba74fc88f7e447e290
unknown <favourgabriel50@gmail.com>
2024-03-03 12:33:39 +0100

    completed CRUD functionalities for Post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    protected $post;
+    protected $user;
+
+    public function __construct(Post $post, User $user){
+        $this->post = $post;
+        $this->user = $user;
+    }
+
+    public function createPost(Request $request){
+         $request->validate([
+            'status' => 'required',
+            'status' => 'required',
+        ]);
+
+        $postDetails = [
+            'title' => $request->input('title'),
+            'dob' => $request->input('dob'),
+        ];
+
+        if(!$this->post->create($postDetails)){
+            return [
+                'status' => 'failed',
+                'status_code' => 400,
+                'message' => 'post post was not created'
+            ];
+        }
+    }
+
+
+    public function getPost($postId){
+        if(!$post = $this->post->find($postId)){
+            return [
+                'status' => 'failed',
+                'status_code' => 400,
+                'message' => 'Could not get User post'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => $post
+        ];
+    }
+
+    public function updatePost($postId, Request $request){
+        if(!$post = $this->post->find($postId)){
+            return [
+                'status' =>'failed',
+                'status_code' => 400,
+                'message' => 'Failed to Update',
+            ];
+        }
+
+        $postDetails = [
+            'title'=> $request->title,
+            'body'=> $request->body,
+        ];
+
+
+        $post->update($postDetails);
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'data' => $post,
+        ];
+    }
+
+    public function deletePost($postId){
+        $post= $this->post->find($postId);
+
+        if(!$post){
+            return [
+                'status' =>'failed',
+                'message' => 'Could not find Post',
+            ];
+        }
+
+        $post->delete();
+
+        return [
+            'status' =>'success',
+            'status_code' => 200,
+            'message' => 'Post Deleted',
+        ];
+    }
+}
